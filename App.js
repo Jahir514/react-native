57ca76248187674d4a3ce0b716fe8206d9c30f56
Jahir514 <jahirhasan.amana@gmail.com>
2023-06-18 14:58:08 +0600

    Start guessing game

--- App.js
+++ App.js
@@ -1,61 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, View, FlatList, Button } from 'react-native';
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
-export default function App() {
-  const [modalIsVisible, setModalIsVisible] = useState(false);
-  const [goalLists, setGoalLists] = useState([]);
-  const startAddGoalHandler = () => {
-    setModalIsVisible(true);
-  };
-  const endAddGoalHandler = () => {
-    setModalIsVisible(false);
-  };
-  const goalAddHandler = (inputText) => {
-    setGoalLists((prevInputText) => [
-      ...prevInputText,
-      { text: inputText, id: Math.random().toString() },
-    ]);
-  };
+import { StyleSheet, View, FlatList, Button, Text } from 'react-native';
 
-  const deleteGoalHandler = (id) => {
-    setGoalLists((currentGoals) => {
-      return currentGoals.filter((goal) => goal.id != id);
-    });
-  };
+export default function App() {
 
   return (
     <>
-      <StatusBar style='light' />
+      <StatusBar />
       <View style={styles.appContainer}>
-        <Button
-          title='Add New Goal'
-          color='#5e08cc'
-          onPress={startAddGoalHandler}
-        />
-        <GoalInput
-          goalAddHandler={goalAddHandler}
-          goalCancelHandler={endAddGoalHandler}
-          modalVisible={modalIsVisible}
-        />
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={goalLists}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  itemData={itemData}
-                  deleteGoalHandler={deleteGoalHandler}
-                />
-              );
-            }}
-            alwaysBounceHorizontal={false}
-            keyExtractor={(item, index) => {
-              return item.id;
-            }}
-          />
-        </View>
+        <Text>Gueessing Game</Text>
       </View>
     </>
   );
@@ -66,8 +18,5 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
     flex: 1,
-  },
-  goalsContainer: {
-    flex: 6,
-  },
+  }
 });
